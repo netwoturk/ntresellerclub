579fe6ef65a14e9c2a3b6953e7eb19edebfd4a94
netwoturk <96207693+netwoturk@users.noreply.github.com>
2026-06-23 16:13:57 +0300

    Show calculated TR sale prices in admin table

--- prestashop/ntresellerclub/classes/NtRcTrPriceAdminRenderer.php
+++ prestashop/ntresellerclub/classes/NtRcTrPriceAdminRenderer.php
@@ -4,6 +4,7 @@ if (!defined('_PS_VERSION_')) {
 }
 
 require_once __DIR__ . '/NtRcTrPriceManager.php';
+require_once __DIR__ . '/NtRcTrPriceCalculator.php';
 
 class NtRcTrPriceAdminRenderer
 {
@@ -13,15 +14,20 @@ class NtRcTrPriceAdminRenderer
         $html = '<div class="panel"><h3>TR Domain Fiyatlari</h3>';
         $html .= '<p>DomainNameAPI sadece TR uzantilari icin kullanilir. Global domainler ResellerClub uzerinden calisir.</p>';
         $html .= '<table class="table"><thead><tr>';
-        $html .= '<th>Uzanti / Islem</th><th>Alis</th><th>Doviz</th><th>Satis</th><th>Mod</th><th>Yuzde</th><th>Sabit</th><th>Son Sync</th>';
+        $html .= '<th>Uzanti / Islem</th><th>Alis</th><th>Doviz</th><th>Kur Sonrasi Maliyet</th><th>Musteri Satis</th><th>Mod</th><th>Yuzde</th><th>Sabit</th><th>Son Sync</th>';
         $html .= '</tr></thead><tbody>';
 
         foreach ((array)$rows as $row) {
+            $calc = NtRcTrPriceCalculator::calculate($row);
+            $costConverted = !empty($calc['success']) ? $calc['cost_converted'] . ' ' . $calc['target_currency'] : 'Kur yok';
+            $salePrice = !empty($calc['success']) ? $calc['sale_price'] . ' ' . $calc['target_currency'] : '-';
+
             $html .= '<tr>';
             $html .= '<td>' . Tools::safeOutput($row['code']) . '</td>';
             $html .= '<td>' . Tools::safeOutput($row['cost_price']) . '</td>';
             $html .= '<td>' . Tools::safeOutput($row['currency']) . '</td>';
-            $html .= '<td>' . Tools::safeOutput($row['sale_price']) . '</td>';
+            $html .= '<td>' . Tools::safeOutput($costConverted) . '</td>';
+            $html .= '<td><strong>' . Tools::safeOutput($salePrice) . '</strong></td>';
             $html .= '<td>' . Tools::safeOutput($row['margin_mode']) . '</td>';
             $html .= '<td>' . Tools::safeOutput($row['margin_percent']) . '</td>';
             $html .= '<td>' . Tools::safeOutput($row['margin_fixed']) . '</td>';
@@ -30,7 +36,7 @@ class NtRcTrPriceAdminRenderer
         }
 
         if (!$rows) {
-            $html .= '<tr><td colspan="8">Henuz TR domain fiyat kaydi yok.</td></tr>';
+            $html .= '<tr><td colspan="9">Henuz TR domain fiyat kaydi yok.</td></tr>';
         }
 
         $html .= '</tbody></table>';
