d6513673a407dc579674edfdac910f277caae718
netwoturk <96207693+netwoturk@users.noreply.github.com>
2026-06-25 17:23:37 +0300

    feat: add BTK CSV feature license key

--- prestashop/ntresellerclub/classes/NtRcLicense.php
+++ prestashop/ntresellerclub/classes/NtRcLicense.php
@@ -5,6 +5,8 @@ if (!defined('_PS_VERSION_')) {
 
 class NtRcLicense
 {
+    const FEATURE_BTK_CSV_REPORTING = 'btk_csv_reporting';
+
     public static function normalizeDomain($domain)
     {
         $domain = strtolower(trim((string)$domain));
@@ -13,8 +15,27 @@ class NtRcLicense
         return $parts[0];
     }
 
+    public static function hasFeature($featureKey)
+    {
+        if (!self::isActive()) {
+            return false;
+        }
+
+        $featureKey = self::normalizeFeatureKey($featureKey);
+        if ($featureKey === self::FEATURE_BTK_CSV_REPORTING) {
+            return (bool)Configuration::get('NTRC_FEATURE_BTK_CSV_REPORTING');
+        }
+
+        return false;
+    }
+
     public static function isActive()
     {
         return true;
+    }
+
+    protected static function normalizeFeatureKey($featureKey)
+    {
+        return strtolower(trim((string)$featureKey));
     }
 }
