46f3eb8f3bd5509980a33ca58573855daac51fe0
netwoturk <96207693+netwoturk@users.noreply.github.com>
2026-06-30 22:07:37 +0300

    Restore TR domain admin read only list

--- prestashop/ntresellerclub/controllers/admin/AdminNtRcTrDomainsController.php
+++ prestashop/ntresellerclub/controllers/admin/AdminNtRcTrDomainsController.php
@@ -4,8 +4,14 @@ if (!defined('_PS_VERSION_')) {
 }
 
 require_once _PS_MODULE_DIR_ . 'ntresellerclub/classes/admin/NtRcAdminBaseController.php';
+require_once _PS_MODULE_DIR_ . 'ntresellerclub/controllers/admin/AdminNtRcDomainsController.php';
 
-class AdminNtRcTrDomainsController extends NtRcAdminBaseController
+class AdminNtRcTrDomainsController extends AdminNtRcDomainsController
 {
     protected $ntRcSection = 'tr_domains';
+
+    protected function renderSectionContent()
+    {
+        return $this->renderDomainServiceList('tr_domain');
+    }
 }
