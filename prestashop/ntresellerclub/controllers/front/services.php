285fc735a51d6953fb4386f3386bb334bc35c15a
netwoturk <96207693+netwoturk@users.noreply.github.com>
2026-06-30 22:07:30 +0300

    Restore customer domain services list

--- prestashop/ntresellerclub/controllers/front/services.php
+++ prestashop/ntresellerclub/controllers/front/services.php
@@ -9,7 +9,16 @@ class NtresellerclubServicesModuleFrontController extends ModuleFrontController
         parent::initContent();
 
         $items = Db::getInstance()->executeS(
-            'SELECT * FROM `' . _DB_PREFIX_ . 'ntresellerclub_service` WHERE id_customer=' . (int)$this->context->customer->id . ' ORDER BY expiry_date ASC'
+            'SELECT s.*, q.status AS queue_status '
+            . 'FROM `' . _DB_PREFIX_ . 'ntresellerclub_service` s '
+            . 'LEFT JOIN `' . _DB_PREFIX_ . 'ntresellerclub_operation_queue` q ON q.id_service = s.id_ntresellerclub_service '
+            . 'AND q.id_ntresellerclub_operation_queue = ('
+                . 'SELECT MAX(q2.id_ntresellerclub_operation_queue) FROM `' . _DB_PREFIX_ . 'ntresellerclub_operation_queue` q2 '
+                . 'WHERE q2.id_service = s.id_ntresellerclub_service'
+            . ') '
+            . 'WHERE s.id_customer=' . (int)$this->context->customer->id . ' '
+            . 'AND s.service_type IN ("domain", "tr_domain") '
+            . 'ORDER BY s.created_at DESC'
         );
 
         foreach ((array)$items as &$item) {
@@ -18,9 +27,7 @@ class NtresellerclubServicesModuleFrontController extends ModuleFrontController
             ));
         }
 
-        $this->context->smarty->assign(array(
-            'nt_services' => is_array($items) ? $items : array(),
-        ));
+        $this->context->smarty->assign(array('nt_services' => is_array($items) ? $items : array()));
 
         $this->setTemplate('module:ntresellerclub/views/templates/front/services.tpl');
     }
